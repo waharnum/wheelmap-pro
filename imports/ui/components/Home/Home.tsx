dc97c41d9eb453566f2bb4ee199b4449bbddb535
Thomas Mann <thomas.mann@framefield.com>
2017-08-25 17:28:11 +0200

    Added simpl-schema

--- imports/ui/components/Home/Home.tsx
+++ imports/ui/components/Home/Home.tsx
@@ -2,9 +2,12 @@ import * as React from 'react';
 import styled, { keyframes } from 'styled-components';
 
 const Home = (props) => (
-    <div className={`${props.className}`}>
-        <h1>Home!</h1>
+  <div className={`${props.className}`}>
+    <h1>Home!</h1>
+    <div className="alert alert-primary" role="alert">
+      This is a primary alert—check it out!
     </div>
+  </div>
 );
 
 export default styled(Home) `
