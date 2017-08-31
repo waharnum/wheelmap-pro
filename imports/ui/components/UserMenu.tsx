8b6371a799b6b716ac19cd947b407f9933df53dd
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-08-31 20:20:07 +0200

    Added correct avatar and email for current user

--- imports/ui/components/UserMenu.tsx
+++ imports/ui/components/UserMenu.tsx
@@ -1,12 +1,19 @@
-import * as React from 'react';
-import {Link} from 'react-router';
 import styled from 'styled-components';
+import {Link} from 'react-router';
+import * as React from 'react';
 
 import { IStyledComponent } from '../components/IStyledComponent';
+import { getGravatarImageUrl } from '../../both/lib/user-icon';
+import { getDisplayedNameForUser } from '../../both/lib/user-name';
 
-const UserMenu = (props : IStyledComponent) => {
+// TODO: needs binding to current user
+
+const UserMenu = (props: IStyledComponent) => {
   return (
-    <li className={props.className}><Link to="/profile">Alex Bright</Link></li>
+    <li className={props.className}>
+      <img src={getGravatarImageUrl(Meteor.user().profile.gravatarHash)} className="user-icon" />
+      <Link to="/profile">{getDisplayedNameForUser(Meteor.user())}</Link>
+    </li>
   );
 };
 
@@ -18,6 +25,11 @@ export default styled(UserMenu) `
 
   a {
     font-size: 14px;
+  }
+
+  img {
+    width: 24px;
+    padding-right: 4px;
   }
   
   padding-right: 26px;
