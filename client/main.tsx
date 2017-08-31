ef436c6d865d38831099c1f7093d2e63cf7f4dcd
Rainer Mühr <rainer.muehr@framefield.com>
2017-08-31 17:49:49 +0200

    CSS-structure refactored

--- client/main.tsx
+++ client/main.tsx
@@ -3,7 +3,12 @@ import { ReactRouterSSR } from 'meteor/reactrouter:react-router-ssr';
 
 import AppRouter from '../imports/ui/AppRouter';
 import '../imports/both/api/users/accounts';
-import './main.scss';
+
+// import './main.scss';
+import '../imports/ui/stylesheets/colors.scss';
+import '../imports/ui/stylesheets/fonts.scss';
+import '../imports/ui/stylesheets/common.scss';
+import '../imports/ui/stylesheets/forms.scss';
 
 Meteor.startup(() => {
   ReactRouterSSR.Run(AppRouter);
