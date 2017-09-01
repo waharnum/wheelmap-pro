ac0fdb737c558cda9d2391a4da4be6dbdc8c76f5
Rainer Mühr <rainer.muehr@framefield.com>
2017-09-01 15:35:45 +0200

    more styling to organize events

--- imports/ui/pages/Events/OrganizeEventPage.tsx
+++ imports/ui/pages/Events/OrganizeEventPage.tsx
@@ -16,10 +16,9 @@ import EventTabs from './EventTabs';
 
 const OrganizeEventPage = (props: IModelProps<IEvent> & IStyledComponent) => {
   const model = props.model || {
-    _id: '123',
-    organizationId: '123',
-    name: 'Some mapathon',
-    description: 'asj dflkjsdf ;lajksdf;l akjdfadf',
+    _id: "123",
+    name: "Mapathon Montreal",
+    description: 'We are an international organization for medical emergency relief. We provide medical emergency assistance in crisis and war zones. We collect medical facilities such as doctors, pharmacies and hospitals.',
     regionName: 'Montreal',
     startTime: new Date('2017-10-10 12:10:12'),
     endTime: new Date('2017-10-10 15:10:12'),
@@ -46,22 +45,38 @@ const OrganizeEventPage = (props: IModelProps<IEvent> & IStyledComponent) => {
         tabs={(<EventTabs />)}
       />
       <div className="content-area scrollable">
+        <div className="event-stats">
+          <section className="participant-stats">
+            <span className="participants-invited">0<small>invited</small></span>
+            <span className="participants-registered key-figure">0<small>registered</small></span>
+          </section>
+          <section className="event-countdown">
+            <span className="days-countdown">27<small>days</small></span>
+            <span className="hours-countdown">11<small>hours</small></span>
+            <span className="minutes-countdown">13<small>minutes</small></span>
+            <span className="seconds-countdown">10<small>seconds</small></span>
+          </section>
+          <section className="location-stats">
+            <span className="locations-planned">0<small>planned</small></span>
+            <span className="locations-mapped key-figure">0<small>mapped</small></span>
+          </section>
+        </div>
         <ol className="event-timeline before-event">
-          <li className="event-timeline-step event-creation completed">
+          <li className="event-timeline-step event-creation completed finished-last">
             <div className="event-creation-head">
-              <h3>Event created successfully</h3>
-            </div>
-            <div className={props.className || ''}>
-              <div>{model.name}</div>
-              <div>{model.description}</div>
-              <div>9. September 2017</div>
-              <div>{model.regionName}</div>
+              <h3>Event created successfully.</h3>
               <Button to={`/events/edit/${model._id}`}>Edit</Button>
+            </div>
+            <div className={props.className+ " event-details"}>
+              <div className="event-name">{model.name}</div>
+              <div className="event-description">{model.description}</div>
+              <div className="event-date">9. September 2017</div>
+              <div className="event-location">{model.regionName}</div>
             </div>
           </li>
           <li className="event-timeline-step invite-participants active">
             <h3>No participants invited.</h3>
-            <Button to={`/events/edit/${model._id}`}>Invite participants</Button>
+            <Button className="btn-primary" to={`/events/edit/${model._id}`}>Invite participants</Button>
           </li>
           <li className="event-timeline-step organizer-tips enabled">
             <h3>Tips for event organizers</h3>
@@ -92,14 +107,95 @@ const OrganizeEventPage = (props: IModelProps<IEvent> & IStyledComponent) => {
 };
 
 export default styled(OrganizeEventPage) `
+
+.content-area {
+  padding-top: 0;
+  padding-left: 0; /* to have a marginless stats bar */
+  padding-right: 0; /* to have a marginless stats bar */
+}
+
+.event-stats {
+  padding-top: 20px;
+  background-color: white;
+  border-bottom: 1px solid ${colors.shadowGrey};
+  display: flex;
+  justify-content: space-between;
+  
+  section {
+    padding: 0px 20px 0 20px;
+    text-align: center;
+    display: flex;
+
+    span {
+      position: relative;
+      padding: 0 10px 16px 10px;
+      font-size: 30px;
+      line-height: 30px;
+      font-weight: 200;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      &.key-figure {
+        font-size: 32px;
+        font-weight: 800;
+      }
+
+      small {
+        font-size: 11px;
+        line-height: 11px;
+        font-weight: 300;
+        text-transform: uppercase;
+      }
+    }
+
+    &:before {
+      position: relative;
+      top: 2px;
+      left: 0;
+      width: 27px;
+      height: 27px;
+      content: " ";
+      background-image: url(/images/icon-person@2x.png); 
+      background-position: center center;
+      background-repeat: no-repeat;
+      background-size: 100% 100%;
+    }
+  }
+
+  section.participant-stats {
+    border-right: 1px solid ${colors.shadowGrey};
+    
+    &:before {
+      background-image: url(/images/icon-person@2x.png); 
+    }
+  }
+
+  section.event-countdown {
+
+    &:before {
+      background-image: url(/images/icon-date@2x.png); 
+    }
+  }
+  
+  section.location-stats {
+    border-left: 1px solid ${colors.shadowGrey};
+
+    &:before {
+      background-image: url(/images/icon-location@2x.png); 
+    }
+  }
+}
+
 ol.event-timeline {
-  margin-left: 20px;
+  margin-left: 40px;
+  margin-right: 20px;
   margin-bottom: 0px;
   padding-bottom: 30px;
   border-left: 2px solid rgba(0, 0, 0, 0.1);
 
   &:first-child {
-    margin-top: 20px;
+    padding-top: 20px;
   }
 
   h2,
@@ -193,19 +289,20 @@ ol.event-timeline {
     }
   }
 
-  li.completed {
-    color: white;
-    background-color: ${colors.doneGreen};
+  li.completed:before {
+    content: "";
+    color: ${colors.doneGreen};
+  }
+  
+  li.finished-last.event-creation {
 
     &:before,
-    &:after { 
-      color: white;
+    &:after,
+    .event-creation-head,
+    .event-creation-head a {
       opacity: 1;
+      color: white;
       background-color: ${colors.doneGreen};
-    }
-    
-    &:before {
-      content: "";
     }
 
     h3 {
@@ -214,7 +311,7 @@ ol.event-timeline {
   }
 
   li.active {
-
+    
     &:before,
     &:after {
       color: #F5A623;
@@ -228,6 +325,10 @@ ol.event-timeline {
     h3 {
       color: #F5A623;
       font-weight: 400;
+    }
+
+    a.btn-primary {
+      display: block;
     }
   }
 
@@ -251,11 +352,82 @@ ol.event-timeline {
 
     .event-creation-head {
       padding: 20px 20px 20px 48px;
-      color: white;
-      background-color: ${colors.doneGreen};
       border-radius: 4px 4px 0 0;
+      display: flex;
+      justify-content: space-between;
+
+      a { 
+        padding: 0;
+        color: ${colors.ctaBlue};
+        background-color: transparent;
+      }
+    }
+    
+    .event-details {
+      padding: 0;
+
+      div {
+        position: relative;
+        padding-left: 48px;
+        border-bottom: 1px solid ${colors.shadowGrey};
+        font-size: 21px;
+        font-weight: 300;
+        
+        &:first-child,
+        &:last-child {
+          border-bottom: none;
+        }
+
+        &:before {
+          position: absolute;
+          left: 16px;
+          width: 20px;
+          height: 20px;
+          content: "C";
+          color: ${colors.bgAnthracite};
+          opacity: 0.5;
+          font-family: "iconfield-v03";
+          font-size: 21px;
+          text-align: center;
+          }
+      }
+
+      .event-name {
+        padding-bottom: 0;
+
+        &:before {
+          content: "I";
+        }
+      }
+
+      .event-description {
+        padding-top: 0;
+        padding-left: 50px;
+        font-size: 14px;
+
+        &:before {
+          content: "";
+        }
+      }
+
+      .event-date {
+        &:before {
+          content: "˙";
+        }
+
+      }
+
+      .event-location { 
+        &:before {
+          content: "y";
+        }
+      }
     }
   }
+}
+
+ol.event-timeline.before-event {
+  padding-top: 20px;
 }
 
 `;
