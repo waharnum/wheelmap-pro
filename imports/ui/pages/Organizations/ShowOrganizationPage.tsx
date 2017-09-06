83759c457e2e3525216c7e569abdfa72a656d285
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-09-06 15:58:38 +0200

    Display latest event in ShowOrganizationPage

--- imports/ui/pages/Organizations/ShowOrganizationPage.tsx
+++ imports/ui/pages/Organizations/ShowOrganizationPage.tsx
@@ -1,5 +1,6 @@
-import * as React from 'react';
 import styled from 'styled-components';
+import * as React from 'react';
+import * as moment from 'moment';
 
 import MapLayout from '../../layouts/MapLayout';
 
@@ -19,20 +20,34 @@ interface IPageModel {
 };
 
 const ShowOrganizationPage = (props: IAsyncDataByIdProps<IPageModel> & IStyledComponent) => {
+  const organization = props.model.organization;
+  const event = props.model.event;
+
   return (
     <MapLayout className={props.className}>
       <PublicHeader
         titleComponent={(
           <HeaderTitle
-            title={props.model.organization.name}
-            logo={props.model.organization.logo}
-            description={props.model.organization.description}
+            title={organization.name}
+            logo={organization.logo}
+            description={organization.description}
           />
         )}
-        organizeLink={`/organizations/${props.model.organization._id}/organize`}
+        organizeLink={`/organizations/${organization._id}/organize`}
       />
       <div className="content-area">
         <AutoSizedStaticMap />
+        {event ?
+        (<div className="event-box">
+          <div className="event-body">
+            <h3>{event.name} ({event.status})</h3>
+            <div>{moment(event.startTime).format('LLLL')}</div>
+            <div>{event.regionName}</div>
+            <div>{moment(event.startTime).diff(moment(), 'days')} Days Left</div>
+            <Button to={`/events/${event._id}`}>Join Us</Button>
+          </div>
+        </div>) : null
+        }
       </div>
     </MapLayout>
   );
@@ -42,13 +57,26 @@ const ReactiveShowOrganisationPage = reactiveSubscriptionById(
   wrapDataComponent<IPageModel,
       IAsyncDataByIdProps<IPageModel | null>,
       IAsyncDataByIdProps<IPageModel>>(ShowOrganizationPage),
-  (id ) => {
+  (id) => {
     const organization = Organizations.findOne(id);
     // fetch model with organization & events in one go
     // TODO: limit this to the actual ongoing events
-    return { organization, events: organization ? organization.getEvents() : [] };
+    return organization ? {organization, event: organization.getEvents()[0]} : null;
   },
   'organizations.by_id.public', 'events.by_organizationId.private');
 
 export default styled(ReactiveShowOrganisationPage) `
+  .content-area {
+    justify-content: center;
+    align-content: center;
+    display: flex;
+  }
+
+  .event-box {
+    margin: auto;
+    position: relative;
+    background: white;
+    padding: 20px 30px;
+    box-shadow: 0 0 7px 1px rgba(0,0,0,0.4);
+  }
 `;
