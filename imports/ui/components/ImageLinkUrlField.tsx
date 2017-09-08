1d77dc01b3c4c1b535db92c4aa84f9aebd39272d
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-09-08 17:22:43 +0200

    Fixed styling of ImageLinkUrlField component

--- imports/ui/components/ImageLinkUrlField.tsx
+++ imports/ui/components/ImageLinkUrlField.tsx
@@ -15,14 +15,16 @@ const ImageLinkUrl = class extends React.Component<any, {validImage: boolean}> {
   }
 
   public render() {
+    const { className, ...remainingProps } = this.props;
+
     return (
-      <section className={this.props.className}>
-        <TextField {...this.props} type="url" />
+      <div className={'field form-group ' + className}>
+        <TextField {...remainingProps} type="url" />
         <div className={this.state.validImage ? 'preview-image-area' : 'hidden-image-area'}>
           <img src={this.props.value} onError={this.imageError} onLoad={this.imageOkay} />
           <img className="image-okay-mark" src="/images/image-okay.png" />
         </div>
-      </section>
+      </div>
     );
   }
 
@@ -51,9 +53,10 @@ export default styled(ImageLinkUrlField) `
     box-shadow: 0 0 7px 1px rgba(0,0,0,0.4);
     margin-left: 30px;
     position: relative;
+    max-width: 400px;
 
     img {
-      max-width: 400px;
+      max-width: 100%;
       max-height: 400px;
     }
 
