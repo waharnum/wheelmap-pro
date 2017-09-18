8ed7d836978d617ae95937e589de9db5592bcb75
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-09-18 15:16:55 +0200

    Added typings for advanced account functions

--- typings/accounts-ui.d.ts
+++ typings/accounts-ui.d.ts
@@ -2,3 +2,13 @@ declare module 'meteor/std:accounts-ui' {
   export const Accounts;
   export const STATES;
 }
+
+declare module 'meteor/accounts-base' {
+  module Accounts {
+    export function registerLoginHandler(name: string, hook: (options: any) => any | undefined);
+
+    export function insertUserDoc(options: any, user: Meteor.User);
+
+    export function onLogout(callback: (params: { user: Meteor.User, connection: Meteor.Connection }) => void);
+  }
+}
