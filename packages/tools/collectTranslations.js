27482223c3bef3102ace49109b20cb4c4a49cea0
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-09-26 16:41:29 +0200

    Added missing translation collection script

---
+++ packages/tools/collectTranslations.js
@@ -0,0 +1,33 @@
+const gulp = require("gulp");
+const ts = require("gulp-typescript");
+const babel = require("gulp-babel");
+const debug = require('gulp-debug');
+const filter = require('gulp-filter');
+
+gulp.task("ts-babel", function () {
+  const tsProject = ts.createProject(__dirname + "/../../tsconfig.json", {
+    "module": "es2015",
+    "target": "es2015",
+    "jsx": "react",
+    "experimentalDecorators": true,
+    "emitDecoratorMetadata": true
+  });
+  return gulp.src([
+    "+(imports|server|client)/**/*.+(js|ts|tsx|jsx)",], {base: __dirname + "/../.."})
+  .pipe(tsProject())
+  .pipe(babel({
+    "plugins": [
+      [
+        "c-3po",
+        {
+          "extract": {
+            "output": __dirname + "/../../used-strings.pot"
+          }
+        }
+      ]
+    ]
+  }))
+  .pipe(debug());
+});
+
+gulp.start('ts-babel');
