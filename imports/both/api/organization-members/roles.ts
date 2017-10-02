e5b0c9fd6aacdede0fcb16c2482fe31f22bb7d2b
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-10-02 17:16:26 +0200

    Ensure roles never get changed

--- imports/both/api/organization-members/roles.ts
+++ imports/both/api/organization-members/roles.ts
@@ -2,13 +2,13 @@ import {t} from 'c-3po';
 
 export type RoleType = 'manager' | 'developer' | 'founder' | 'volunteer' | 'member';
 
-export const roles = [
+export const roles = Object.freeze([
   {label: t`Manager`, value: 'manager'},
   {label: t`Developer`, value: 'developer'},
   {label: t`Founder'`, value: 'founder'},
   {label: t`Volunteer`, value: 'volunteer'},
   {label: t`Member`, value: 'member'},
-];
+]) as Array<{ label: string, value: RoleType }>;
 
 export function getLabelForRole(role: RoleType): string {
   const found = roles.find((re) => {
