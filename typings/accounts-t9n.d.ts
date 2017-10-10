b1532176d161c12329d21cffd332ccdf79fa06b8
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-10-10 12:25:59 +0200

    Added get for t9 api typings

--- typings/accounts-t9n.d.ts
+++ typings/accounts-t9n.d.ts
@@ -1,5 +1,7 @@
 declare module 'meteor/softwarerero:accounts-t9n' {
   module T9n {
     export function setLanguage(language: string): void;
+
+    export function get(key: string): string;
   }
 }
