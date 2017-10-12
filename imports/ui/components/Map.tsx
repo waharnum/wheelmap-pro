cdcbf3ce4734f360e0e110b41a89824c351bb8b2
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-10-12 10:54:05 +0200

    Bumped map package to 1.0

--- imports/ui/components/Map.tsx
+++ imports/ui/components/Map.tsx
@@ -7,17 +7,15 @@ import config from 'wheelmap-react/lib/lib/config';
 import styled from 'styled-components';
 import {IStyledComponent} from './IStyledComponent';
 
-config.mapboxAccessToken = Meteor.settings.public.mapbox;
-config.wheelmapApiKey = Meteor.settings.public.wheelmap;
-config.accessibilityCloudAppToken = Meteor.settings.public.accessibilityCloud;
-
-
 class Map extends React.Component<IStyledComponent> {
   public render(): JSX.Element {
     return (
       <ReactWheelmapMap
         className={`wheelmap-map ${this.props.className}`}
         data-component="Map"
+        mapboxTileUrl={`https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${Meteor.settings.public.mapbox}`}
+        accessibilityCloudAppToken={Meteor.settings.public.accessibilityCloud}
+        wheelmapApiKey={Meteor.settings.public.wheelmap}
         accessibilityFilter={[]}
         toiletFilter={[]}
       />
