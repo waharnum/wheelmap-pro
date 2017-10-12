c9ae427b1bf18a30cc8a1a328cea44df6c3559c9
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-10-12 17:36:43 +0200

    Set some coordinates and zoom level, but still zoom issues persist

--- imports/ui/components/Map.tsx
+++ imports/ui/components/Map.tsx
@@ -13,6 +13,9 @@ class Map extends React.Component<IStyledComponent> {
       <ReactWheelmapMap
         className={`wheelmap-map ${this.props.className}`}
         data-component="Map"
+        zoom={12}
+        lat={53}
+        lon={13}
         mapboxTileUrl={`https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${Meteor.settings.public.mapbox}`}
         accessibilityCloudAppToken={Meteor.settings.public.accessibilityCloud}
         wheelmapApiKey={Meteor.settings.public.wheelmap}
