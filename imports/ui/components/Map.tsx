4dd6064ab4fbfa826a0f311bb52a0222bdde68a0
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-10-17 11:13:18 +0200

    Updated wheelmap-react map to 1.1.0
    - Fixed css issues that happened due to wrong import order
    - Made sure correct arguments are passed for all zoom values
    - Ensured that POIs are correctly created on layers
    - Removed wheelmap pois
    - Ensured that POIs are not filtered by default by setting correct toilet/accessibility filters

--- imports/ui/components/Map.tsx
+++ imports/ui/components/Map.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 
 import ReactWheelmapMap from 'wheelmap-react/lib/components/Map/Map';
+import HighlightableMarker from 'wheelmap-react/lib/components/Map/HighlightableMarker';
+import {yesNoLimitedUnknownArray, yesNoUnknownArray} from 'wheelmap-react/lib/lib/Feature';
+
+import 'leaflet/dist/leaflet.css';
+import 'leaflet.locatecontrol/src/L.Control.Locate.scss';
 import 'wheelmap-react/src/Map.css'
 
-import config from 'wheelmap-react/lib/lib/config';
 import styled from 'styled-components';
 import {IStyledComponent} from './IStyledComponent';
 
@@ -13,20 +17,48 @@ class Map extends React.Component<IStyledComponent> {
       <ReactWheelmapMap
         className={`wheelmap-map ${this.props.className}`}
         data-component="Map"
-        zoom={12}
-        lat={53}
-        lon={13}
+        minZoomWithSetCategory={13}
+        minZoomWithoutSetCategory={16}
+        zoom={10}
+        maxZoom={19}
+        lat={52.541017}
+        lon={13.38609}
+        wheelmapApiBaseUrl={false}
         mapboxTileUrl={`https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${Meteor.settings.public.mapbox}`}
         accessibilityCloudAppToken={Meteor.settings.public.accessibilityCloud}
+        accessibilityCloudTileUrl={`https://www.accessibility.cloud/place-infos?x={x}&y={y}&z={z}&appToken=${Meteor.settings.public.accessibilityCloud}`}
         wheelmapApiKey={Meteor.settings.public.wheelmap}
-        accessibilityFilter={[]}
-        toiletFilter={[]}
+        accessibilityFilter={[].concat(yesNoLimitedUnknownArray)}
+        toiletFilter={[].concat(yesNoUnknownArray)}
+        locateTimeout={500}
+        pointToLayer={this.createMarkerFromFeature}
       />
     );
+  }
+
+  private onMarkerClick = (featureId: string) => {
+    console.log('You clicked the marker', featureId)
+  };
+
+  private createMarkerFromFeature = (feature: any, latlng: [number, number]) => {
+    const properties = feature && feature.properties;
+    if (!properties) {
+      return null;
+    }
+
+    return new HighlightableMarker(latlng, {
+      onClick: this.onMarkerClick,
+      hrefForFeatureId: () => {
+        return ''
+      },
+      feature,
+    });
   }
 };
 
 export default styled(Map)`
-
+  .ac-marker {
+    display: flex !important;
+  }
 `;
 
