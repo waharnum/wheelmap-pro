891b922aa80abc8320fd7d2ace625790ae78ca79
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-11-13 13:25:22 +0100

    Do not display negative Countdown

--- imports/ui/components/Countdown.tsx
+++ imports/ui/components/Countdown.tsx
@@ -2,26 +2,35 @@ import * as React from 'react';
 import * as moment from 'moment';
 
 import {withTime} from './Timed';
+import {Moment} from 'moment';
 
-const CountdownInternal = (props: { start: moment.Moment, now: moment.Moment }) => (
-  <section className="event-countdown">
+type CountdownProps = { start: moment.Moment, moment: moment.Moment };
+
+const CountdownInternal = (props: CountdownProps) => {
+  const isInFuture = props.start.isAfter(props.moment);
+  const first = isInFuture ? props.start : props.moment;
+  const second = isInFuture ? props.moment : props.start;
+
+  return (
+    <section className="event-countdown">
     <span className="days-countdown">
-      {props.start.diff(props.now, 'days')}
+      {first.diff(second, 'days')}
       <small>{'days'}</small>
       </span>
-    <span className="hours-countdown">
-      {props.start.diff(props.now, 'hours') % 24}
-      <small>{'hours'}</small>
+      <span className="hours-countdown">
+      {first.diff(second, 'hours') % 24}
+        <small>{'hours'}</small>
       </span>
-    <span className="minutes-countdown">
-      {props.start.diff(props.now, 'minutes') % 60}
-      <small>'minutes'}</small>
+      <span className="minutes-countdown">
+      {first.diff(second, 'minutes') % 60}
+        <small>'minutes'}</small>
     </span>
-    <span className="seconds-countdown">
-      {props.start.diff(props.now, 'seconds') % 60}
-      <small>{'seconds'}</small>
+      <span className="seconds-countdown">
+      {first.diff(second, 'seconds') % 60}
+        <small>{'seconds'}</small>
     </span>
-  </section>
-);
+    </section>
+  );
+};
 
 export const Countdown = withTime(CountdownInternal, 1000);
