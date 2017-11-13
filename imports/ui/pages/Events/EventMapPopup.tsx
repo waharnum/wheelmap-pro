686777c07d0e87d12b9acbc2b6f96f46673114ce
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-11-13 13:17:15 +0100

    Fixed styling mistake

--- imports/ui/pages/Events/EventMapPopup.tsx
+++ imports/ui/pages/Events/EventMapPopup.tsx
@@ -9,6 +9,7 @@ import EventStatistics from '../Events/EventStatistics';
 import {regionToBbox} from '../../../both/lib/geo-bounding-box';
 import {defaultRegion} from './EventBaseForm';
 import {colors} from '../../stylesheets/colors';
+import {IStyledComponent} from '../../components/IStyledComponent';
 
 interface IEventMapPopupProps {
   event: IEvent;
@@ -19,7 +20,7 @@ interface IEventMapPopupProps {
   onPrevSelected?: () => void;
 }
 
-class EventMapPopup extends React.Component<IEventMapPopupProps> {
+class EventMapPopup extends React.Component<IEventMapPopupProps & IStyledComponent> {
   public render() {
     const event = this.props.event;
 
@@ -28,7 +29,7 @@ class EventMapPopup extends React.Component<IEventMapPopupProps> {
 
     return (
       <CustomMapPopup
-        className="event-marker"
+        className={`event-marker ${this.props.className}`}
         lat={mapPos.lat}
         lon={mapPos.lng}
         additionalLeafletLayers={[L.rectangle(bbox, {
@@ -65,73 +66,129 @@ const BubbleNoseSize = 10;
 
 export default styled(EventMapPopup) `
 
-.event-marker { 
-  text-align: left;
+text-align: left;
 
-  .popup-root:after {
-    content: "";
+.popup-root:after {
+  content: "";
+  position: absolute;
+  //box-shadow: 0px 0px 2px rgba(55,64,77,0.40);
+  box-shadow: 0 5px 20px rgba(0,0,0,0.2);
+  transform: rotate(45deg);
+  bottom: -${BubbleNoseSize}px;
+  left: calc(50% - ${BubbleNoseSize}px);
+  border-width: ${BubbleNoseSize}px;
+  border-style: solid;
+  border-color: transparent #fbfaf9 #fbfaf9 transparent;
+}
+
+.popup-root {
+  pointer-events: auto;
+  padding: 16px 16px 0 16px;
+  background: #fbfaf9;    
+  border-radius: 5px;
+  // box-shadow: 0 0 2px 0 rgba(55,64,77,0.40);
+  box-shadow: 0 5px 20px rgba(0,0,0,0.2);
+  
+  .btn-prev-event, .btn-next-event {
     position: absolute;
-    //box-shadow: 0px 0px 2px rgba(55,64,77,0.40);
-    box-shadow: 0 5px 20px rgba(0,0,0,0.2);
-    transform: rotate(45deg);
-    bottom: -${BubbleNoseSize}px;
-    left: calc(50% - ${BubbleNoseSize}px);
-    border-width: ${BubbleNoseSize}px;
-    border-style: solid;
-    border-color: transparent #fbfaf9 #fbfaf9 transparent;
+    top: calc(50% - 25px);
+    width: 50px;
+    height: 50px;
   }
   
-  .popup-root {
-    pointer-events: auto;
-    padding: 16px 16px 0 16px;
-    background: #fbfaf9;    
-    border-radius: 5px;
-    // box-shadow: 0 0 2px 0 rgba(55,64,77,0.40);
-    box-shadow: 0 5px 20px rgba(0,0,0,0.2);
-    
-    .btn-prev-event, .btn-next-event {
-      position: absolute;
-      top: calc(50% - 25px);
-      width: 50px;
-      height: 50px;
-    }
-    
-    .btn-prev-event {
-      left: -50px;
-    }
-    
-    .btn-next-event {    
-      left: 100%;
-    }
+  .btn-prev-event {
+    left: -50px;
+  }
+  
+  .btn-next-event {    
+    left: 100%;
+  }
 
-    .event-information {
-      display: flex;
-      justify-content: space-between;
-      align-items: flex-start;
-      flex-wrap: wrap;
+  .event-information {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    
+    .event-description {         
       
-      .event-description {         
-        
-        h3,
-        h4 {
-          margin-top: 0px;
-          margin-bottom: 4px;
-          font-size: 21px;
-          font-weight: 300 !important;
-        }
-        h4 {
-          opacity: 0.6;
-        }
+      h3,
+      h4 {
+        margin-top: 0px;
+        margin-bottom: 4px;
+        font-size: 21px;
+        font-weight: 300 !important;
       }
-      
-      .time-until-event {
-        text-align: center;
+      h4 {
+        opacity: 0.6;
+      }
+    }
+    
+    .time-until-event {
+      text-align: center;
 
-        p {
-          margin: 0;
+      p {
+        margin: 0;
+        font-size: 32px;
+        line-height: 32px;
+        font-weight: 200 !important;
+      }
+
+      small {
+        font-size: 11px;
+        line-height: 11px;
+        font-weight: 300;
+        text-transform: uppercase;
+      }
+    } 
+  }
+
+  .event-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+
+    a.btn {
+      margin-bottom: 10px;
+      padding-right: 0;
+    }
+  }
+
+  .event-statistics {
+    padding-top: 20px;
+    background-color: #fbfaf9;
+    display: flex;
+    justify-content: flex-start;
+    z-index: 100;
+    position: relative;
+    
+    &.organization-stats {
+      border-bottom: 1px solid ${colors.shadowGrey};
+    }
+
+    section {
+      padding: 0px 10px 0 10px;
+      text-align: center;
+      border-right: 1px solid ${colors.shadowGrey};
+      display: flex;
+
+      &:last-child {
+        border-right: 0;
+      }
+
+      span {
+        position: relative;
+        padding: 0 10px 16px 10px;
+        font-size: 30px;
+        line-height: 30px;
+        font-weight: 200;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        &.key-figure {
           font-size: 32px;
-          line-height: 32px;
-          font-weight: 200 !important;
+          // font-weight: 800;
         }
 
         small {
@@ -140,88 +197,30 @@ export default styled(EventMapPopup) `
           font-weight: 300;
           text-transform: uppercase;
         }
-      } 
+      }
+
+      &:before {
+        position: relative;
+        top: 2px;
+        left: 0;
+        width: 27px;
+        height: 27px;
+        content: " ";
+        background-image: url(/images/icon-person@2x.png); 
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+      }
     }
 
-    .event-footer {
-      display: flex;
-      justify-content: space-between;
-      align-items: flex-end;
-
-      a.btn {
-        margin-bottom: 10px;
-        padding-right: 0;
-      }
-    }
-
-    .event-statistics {
-      padding-top: 20px;
-      background-color: #fbfaf9;
-      display: flex;
-      justify-content: flex-start;
-      z-index: 100;
-      position: relative;
-      
-      &.organization-stats {
-        border-bottom: 1px solid ${colors.shadowGrey};
-      }
-  
-      section {
-        padding: 0px 10px 0 10px;
-        text-align: center;
-        border-right: 1px solid ${colors.shadowGrey};
-        display: flex;
-  
-        &:last-child {
-          border-right: 0;
-        }
-  
-        span {
-          position: relative;
-          padding: 0 10px 16px 10px;
-          font-size: 30px;
-          line-height: 30px;
-          font-weight: 200;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-  
-          &.key-figure {
-            font-size: 32px;
-            // font-weight: 800;
-          }
-  
-          small {
-            font-size: 11px;
-            line-height: 11px;
-            font-weight: 300;
-            text-transform: uppercase;
-          }
-        }
-  
-        &:before {
-          position: relative;
-          top: 2px;
-          left: 0;
-          width: 27px;
-          height: 27px;
-          content: " ";
-          background-image: url(/images/icon-person@2x.png); 
-          background-position: center center;
-          background-repeat: no-repeat;
-          background-size: 100% 100%;
-        }
-      }
-  
-      /* prefix icons*/
-      section.participant-stats:before { background-image: url(/images/icon-person@2x.png); }
-      section.location-stats:before { background-image: url(/images/icon-location@2x.png); }
-      section.event-stats:before { background-image: url(/images/icon-date@2x.png); }
-      section.new-event:before { 
-        width: 0;
-        height: 0;
-        background-image: none; 
-      }
+    /* prefix icons*/
+    section.participant-stats:before { background-image: url(/images/icon-person@2x.png); }
+    section.location-stats:before { background-image: url(/images/icon-location@2x.png); }
+    section.event-stats:before { background-image: url(/images/icon-date@2x.png); }
+    section.new-event:before { 
+      width: 0;
+      height: 0;
+      background-image: none; 
     }
   }
 }
