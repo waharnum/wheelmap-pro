54b124e673b07d5bdb4ec2dcfee70727c78873d3
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-11-17 15:02:06 +0100

    Disable clicking on disabled react-router Links

--- imports/ui/components/Button.tsx
+++ imports/ui/components/Button.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
-import { LinkProps, Link } from 'react-router';
+import {LinkProps, Link} from 'react-router';
 import styled from 'styled-components';
 
-import { IStyledComponent } from './IStyledComponent';
+import {IStyledComponent} from './IStyledComponent';
 
 const Button = (props: LinkProps & IStyledComponent) => {
-  return (<Link {...props} className={props.className + ' btn'} />);
+  return (<Link {...props} className={props.className + ' btn'}/>);
 };
 
 export default styled(Button) `
+&[disabled] {
+  pointer-events: none;
+}
 `;
