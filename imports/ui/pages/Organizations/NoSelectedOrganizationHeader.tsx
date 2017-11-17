b554267a55c559625098190fdfd23da420555684
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-11-17 11:00:52 +0100

    Fixed empty header when there is no active organization

--- imports/ui/pages/Organizations/NoSelectedOrganizationHeader.tsx
+++ imports/ui/pages/Organizations/NoSelectedOrganizationHeader.tsx
@@ -2,18 +2,13 @@ import {t} from 'c-3po';
 import styled from 'styled-components';
 import * as React from 'react';
 
-import AdminTab from '../../components/AdminTab';
-import ScrollableLayout from '../../layouts/ScrollableLayout';
 import Button from '../../components/Button';
-import AdminHeader from '../../components/AdminHeader';
 import {IStyledComponent} from '../../components/IStyledComponent';
-import {Hint, HintBox} from '../../components/HintBox';
 import {Link} from 'react-router';
 import {
   IAsyncDataProps, reactiveSubscription,
 } from '../../components/reactiveModelSubscription';
-import {wrapDataComponent} from '../../components/AsyncDataComponent';
-import {IOrganization, Organizations} from '../../../both/api/organizations/organizations';
+import {Organizations} from '../../../both/api/organizations/organizations';
 import OrganizationsDropdown from '../../components/OrganizationsDropdown';
 
 const NoSelectedOrganizationHeader = (props: IAsyncDataProps<number> & IStyledComponent) => {
@@ -23,12 +18,11 @@ const NoSelectedOrganizationHeader = (props: IAsyncDataProps<number> & IStyledCo
         <Button to="/organizations/create">{t`Create Organization`}</Button>
       </OrganizationsDropdown>)
   }
-  return (<Link to="/" className={props.className}><h1>{t`wheelmap.pro`}</h1></Link>);
+  return (<Link to="/" className={`${props.className} logo`}><h1>{t`wheelmap.pro`}</h1></Link>);
 }
 
 const ReactiveNoSelectedOrganizationHeader = reactiveSubscription(
-  wrapDataComponent<number, IAsyncDataProps<number | null>,
-    IAsyncDataProps<number>>(NoSelectedOrganizationHeader),
+  NoSelectedOrganizationHeader,
   () => {
     // TODO: align this filter to only display my organizations
     return Organizations.find({}, {sort: {name: 1}}).count()
