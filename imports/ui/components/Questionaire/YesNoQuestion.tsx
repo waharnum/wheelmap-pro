8aa311f382ee6d721cb1d73a4ccc60de3dcb7ba4
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-11-30 17:17:29 +0100

    Added YesNoQuestion component

---
+++ imports/ui/components/Questionaire/YesNoQuestion.tsx
@@ -0,0 +1,35 @@
+import {t} from 'c-3po';
+import styled from 'styled-components';
+import * as React from 'react';
+import connectField from 'uniforms/connectField';
+
+import {IStyledComponent} from '../IStyledComponent';
+
+type Props = {
+  onChange: (value: boolean | null) => void,
+  value: boolean;
+};
+
+const YesNoQuestion = class extends React.Component<IStyledComponent & Props> {
+  public render() {
+    return (
+      <span className={`call-to-action ${this.props.className}`}>
+        <div className='form'>
+          <div className='form-group'>
+              <button className="btn btn-primary" onClick={() => {
+                this.props.onChange(true);
+              }}>{t`Yes`}</button>
+              <button className="btn btn-primary" onClick={() => {
+                this.props.onChange(false);
+              }}>{t`No`}</button>
+          </div>
+        </div>
+      </span>
+    );
+  }
+};
+
+const YesNoQuestionField = connectField(YesNoQuestion);
+
+export default styled(YesNoQuestionField) `
+`;
