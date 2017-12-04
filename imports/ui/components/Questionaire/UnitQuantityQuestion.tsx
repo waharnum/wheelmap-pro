fe0c7b82bb8b33a6f80aae159dab5d38c0bc28bc
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-12-04 22:07:05 +0100

    Allow entering quantities in different unit than the displayed unit

--- imports/ui/components/Questionaire/UnitQuantityQuestion.tsx
+++ imports/ui/components/Questionaire/UnitQuantityQuestion.tsx
@@ -1,11 +1,13 @@
-import {t} from 'c-3po';
 import styled from 'styled-components';
+import * as Qty from 'js-quantities';
 import * as React from 'react';
 import connectField from 'uniforms/connectField';
 
-import {IStyledComponent} from '../IStyledComponent';
 import {determineUnitKind, Quantity} from '@sozialhelden/ac-format';
+
 import {colors} from '../../stylesheets/colors';
+import {IStyledComponent} from '../IStyledComponent';
+import {bestMatchClientLocale} from '../../../../client/i18n';
 import {getPreferredUnitForKind} from '../../../both/i18n/units';
 
 type Props = {
@@ -19,6 +21,8 @@ type State = {
   unitKind: string,
   unit: string, // as preferred by the data entry
   displayUnit: string, // localized
+  displayValue: string, // localized
+  lastValue: Quantity,
 };
 
 const UnitQuantityQuestion = class extends React.Component<IStyledComponent & Props, State> {
@@ -32,20 +36,13 @@ const UnitQuantityQuestion = class extends React.Component<IStyledComponent & Pr
   }
 
   public render() {
-    console.log(this.props);
-
-    // TODO how to handle operators here?
-    const inputValue = (!this.props.value || typeof this.props.value === 'string') ?
-      this.props.value :
-      this.props.value.value;
-
     return (
       <span className={`call-to-action ${this.props.className}`}>
         <div className="form">
           <div className="form-group">
             <section className="inputFieldWrapper">
-              <input type="text"
-                     value={inputValue || ''}
+              <input type="number"
+                     value={this.state.displayValue}
                      className="form-control"
                      placeholder={this.props.placeholder}
                      onChange={this.textChanged}/>
@@ -61,28 +58,50 @@ const UnitQuantityQuestion = class extends React.Component<IStyledComponent & Pr
     const rawValue = event.target.value;
 
     if (this.props.onChange) {
-      // TODO convert from display unit to output unit
-      const asString = `${rawValue}${this.state.displayUnit}`;
+      const displayString = `${rawValue}${this.state.displayUnit}`;
+      // convert from display unit to output unit
+      const storeValue = Qty(displayString).to(this.state.unit).scalar;
 
       this.props.onChange({
         unit: this.state.unit,
-        value: rawValue,
+        value: storeValue,
         // TODO determine accuracy from user tooling
         accuracy: 5,
         toString: () => {
-          return asString;
+          return displayString;
         },
       } as Quantity);
     }
   };
 
   propsChanged(props: Props) {
+    let displayValue = '';
     const unitKind = determineUnitKind(props.field.type as SimpleSchema);
     const preferredUnit = props.field.uniforms ? props.field.uniforms.preferredUnit : '';
+    const unit = getPreferredUnitForKind(unitKind, preferredUnit);
+    const displayUnit = getPreferredUnitForKind(unitKind, preferredUnit, bestMatchClientLocale);
+
+    let lastValue = props.value;
+    if (props.value) {
+      // TODO how to handle operator & accuracy here?
+      if (typeof props.value === 'string') {
+        const parsedQty = Qty(props.value).to(this.state.unit);
+        displayValue = String(parsedQty.scalar);
+        lastValue = {
+          unit: parsedQty.units(),
+          value: parsedQty.scalar,
+        };
+      } else if (props.value.value || props.value.unit) {
+        displayValue = String(Qty(props.value.value || 0, props.value.unit).to(displayUnit).scalar);
+      }
+    }
+
     this.setState({
       unitKind,
-      unit: getPreferredUnitForKind(unitKind, preferredUnit),
-      displayUnit: getPreferredUnitForKind(unitKind, preferredUnit, 'de_DE'),
+      displayValue,
+      lastValue,
+      unit,
+      displayUnit,
     });
   }
 };
