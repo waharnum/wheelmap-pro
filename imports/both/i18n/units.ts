cd61444dad104be0b84a8cc0ebcf222ff452817e
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-12-04 22:03:14 +0100

    Improved preferred unit detection to discern length by locale

--- imports/both/i18n/units.ts
+++ imports/both/i18n/units.ts
@@ -1,7 +1,16 @@
 const data = {
-  'length': 'cm',
+  'length': {
+    'en_UK': 'inch',
+    'en_US': 'inch',
+    '*': 'cm',
+  },
 };
 
-export const getPreferredUnitForKind = (unitKind: string, preferredUnit: string, locale?: string): string => {
-  return data[unitKind] || 'unknown';
+export const getPreferredUnitForKind = (unitKind: string, preferredUnit: string, locale?: string): string | 'unknown' => {
+  if (!data[unitKind]) {
+    console.error(`Could not find unit for ${unitKind}`);
+    return 'unknown';
+  }
+
+  return (locale ? data[unitKind][locale] || data[unitKind]['*'] : data[unitKind]['*']) || 'unknown';
 };
