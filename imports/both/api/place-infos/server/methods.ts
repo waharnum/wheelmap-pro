74b5a995c0371f4aede2366d8142923af94382f6
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-12-06 17:54:37 +0100

    Added remote method for inserting place data

--- imports/both/api/place-infos/server/methods.ts
+++ imports/both/api/place-infos/server/methods.ts
@@ -1,25 +1,38 @@
 import {Meteor} from 'meteor/meteor';
-import * as Fiber from 'fibers';
+import SimpleSchema from 'simpl-schema';
 
-import pickRandomPlaceInfos from './pick-random-place-infos';
-import {IPlaceInfo} from '../place-infos';
+import {IPlaceInfo, PlaceInfos} from '../place-infos';
+import {PlaceInfoSchema} from '@sozialhelden/ac-format';
+import {t} from 'c-3po';
+import {userIsAllowedToMapInEventId} from '../../events/priviledges';
 
-// This number is intentionally not very high even if a higher number of shown places would be more
-// impressive. Less transferred map tile data means happier people with volume flat data plans :)
-const CACHED_PLACE_INFO_COUNT = 25;
+export const insert = new ValidatedMethod({
+  name: 'placeInfos.insertForEvent',
+  validate: new SimpleSchema({
+    eventId: {
+      type: String,
+      regEx: SimpleSchema.RegEx.Id,
+    },
+    place: {
+      type: PlaceInfoSchema,
+    },
+  }).validator(),
+  run(doc: { eventId: Mongo.ObjectID, place: IPlaceInfo }) {
+    console.log('Inserting place', doc.place, 'for user id', this.userId);
 
-let cachedRandomPlaceInfos: IPlaceInfo[] = [];
+    if (!this.userId) {
+      throw new Meteor.Error(401, t`Please log in first.`);
+    }
+    if (!userIsAllowedToMapInEventId(this.userId, doc.eventId)) {
+      throw new Meteor.Error(403, t`You do not have rights to add places for this event.`);
+    }
 
-Meteor.startup(() => {
-  // Pick shown places in a new fiber to get faster Meteor restarts while developing
-  Fiber(() => {
-    cachedRandomPlaceInfos = pickRandomPlaceInfos(CACHED_PLACE_INFO_COUNT);
-  }).run();
-});
+    // TODO fetch or create source for event id
 
-Meteor.methods({
-  'PlaceInfos.getRandomPlaceInfos'() {
-    this.unblock();
-    return cachedRandomPlaceInfos || [];
+    // TODO store user id with place
+
+    (doc.place as any)._testing = true;
+
+    return PlaceInfos.insert(doc.place);
   },
 });
