7dd7e6fe036f414af6219f4e8304ecbb4260ac41
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2018-01-26 16:54:22 +0100

    Added color property per event status enum

--- imports/both/api/events/eventStatus.ts
+++ imports/both/api/events/eventStatus.ts
@@ -1,18 +1,27 @@
 import {t, gettext} from 'c-3po';
+import {colors} from '../../../ui/stylesheets/colors';
 
 export type EventStatusEnum = 'draft' | 'planned' | 'ongoing' | 'completed' | 'canceled';
 
 export const eventStatusLabels = Object.freeze([
-  {label: t`Draft`, value: 'draft'},
-  {label: t`Planned`, value: 'planned'},
-  {label: t`Ongoing`, value: 'ongoing'},
-  {label: t`Completed`, value: 'completed'},
-  {label: t`Canceled`, value: 'canceled'},
-]) as ReadonlyArray<{ label: string, value: EventStatusEnum }>;
+  {label: t`Draft`, value: 'draft', color: colors.ctaDisabledGrey},
+  {label: t`Planned`, value: 'planned', color: colors.linkBlue},
+  {label: t`Ongoing`, value: 'ongoing', color: colors.activeOrange},
+  {label: t`Completed`, value: 'completed', color: colors.ctaGreen},
+  {label: t`Canceled`, value: 'canceled', color: colors.errorRed},
+]) as ReadonlyArray<{ label: string, value: EventStatusEnum, color: string }>;
+
+export function getColorForEventStatus(state: EventStatusEnum | undefined): string {
+  const found = eventStatusLabels.find((re) => {
+    return re.value === state;
+  });
+
+  return found ? found.color : '#ccc';
+}
 
 export function getLabelForEventStatus(state: EventStatusEnum | undefined): string {
   const found = eventStatusLabels.find((re) => {
-    return re.value == state;
+    return re.value === state;
   });
 
   return found ? gettext(found.label) : t`Unknown Status`;
